d515bd85a263ee26fb95b7f68835f3921b2acd00
louiscazenave5-maker <louiscazenave5@gmail.com>
2026-07-16 20:54:18 +0200

    Update order.js
    Signed-off-by: louiscazenave5-maker <louiscazenave5@gmail.com>

--- api/order.js
+++ api/order.js
@@ -1,6 +1,5 @@
 import formidable from "formidable";
 import fs from "fs";
-import PDFDocument from "pdfkit";
 
 export const config = {
     api: {
@@ -124,91 +123,6 @@ export default async function handler(req, res) {
         const orderNumber =
             "LTD-" + Math.floor(100000 + Math.random() * 900000);
 
-      const pdfBuffer = await new Promise((resolve)=>{
-
-    const doc = new PDFDocument();
-
-    let buffers = [];
-
-    doc.on("data", buffers.push.bind(buffers));
-
-    doc.on("end",()=>{
-
-        resolve(Buffer.concat(buffers));
-
-    });
-
-
-    doc.fontSize(22)
-    .fillColor("#071B5A")
-    .text("LTD LS",{
-        align:"center"
-    });
-
-
-    doc.moveDown();
-
-
-    doc.fontSize(14)
-    .fillColor("#D60000")
-    .text("LIMITED GASOLINE",{
-        align:"center"
-    });
-
-
-    doc.moveDown(2);
-
-
-    doc.fillColor("black")
-    .fontSize(12);
-
-
-    doc.text(
-        `Commande : ${orderNumber}`
-    );
-
-
-    doc.text(
-        `Client : ${name}`
-    );
-
-
-    doc.text(
-        `Téléphone : ${phone}`
-    );
-
-
-    doc.text(
-        `Lieu : ${location}`
-    );
-
-
-    doc.moveDown();
-
-
-    doc.text(
-        "Produits :"
-    );
-
-
-    doc.text(
-        products
-    );
-
-
-    doc.moveDown();
-
-
-    doc.fontSize(16)
-    .fillColor("#D60000")
-    .text(
-        `TOTAL : ${total}`
-    );
-
-
-    doc.end();
-
-});
 
 
         const proof =
@@ -328,27 +242,24 @@ export default async function handler(req, res) {
 
         if(proof){
 
-    const buffer = fs.readFileSync(
-        proof.filepath
-    );
+
+            const buffer = fs.readFileSync(
+                proof.filepath
+            );
 
 
-    discordForm.append(
-        "files[0]",
-        new Blob([buffer]),
-        proof.originalFilename || "preuve-paiement.png"
-    );
+            discordForm.append(
 
-}
+                "files[0]",
+
+                new Blob([buffer]),
+
+                proof.originalFilename || "preuve-paiement"
+
+            );
 
 
-// AJOUT DU RECU PDF
-
-discordForm.append(
-    "files[1]",
-    new Blob([pdfBuffer]),
-    `${orderNumber}.pdf`
-);
+        }
 
 
 
