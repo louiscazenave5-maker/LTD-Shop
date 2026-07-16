d5a238f5c27b573a129c401be67914904fe428a2
louiscazenave5-maker <louiscazenave5@gmail.com>
2026-07-16 16:39:19 +0200

    Update order.js
    Signed-off-by: louiscazenave5-maker <louiscazenave5@gmail.com>

--- api/order.js
+++ api/order.js
@@ -255,7 +255,7 @@ export default async function handler(req, res) {
 
                 new Blob([buffer]),
 
-                proof.originalFilename || "preuve.png"
+                proof.originalFilename || "preuve-paiement"
 
             );
 
